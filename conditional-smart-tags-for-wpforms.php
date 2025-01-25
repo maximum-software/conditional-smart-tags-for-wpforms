9a850d936328f0dad1ad1ed8aec1e6541963f04c
Alex <alex@maximum.guru>
2025-01-24 17:59:04 -0800

    Initial commit

---
+++ conditional-smart-tags-for-wpforms.php
@@ -0,0 +1,83 @@
+<?php
+/**
+* Plugin Name: Conditional Smart Tags for WPForms
+* Plugin URI: https://wordpress.org/plugins/conditional-smart-tags-for-wpforms
+* Description: Adds a custom if-then-else smart tag syntax to conditionally show or hide content based on a field value in WPForms.
+* Version: 1.0.0
+* Requires at least: 5.2
+* Requires PHP: 7.2
+* Author: Maximum.Software
+* Author URI: https://maximum.software
+* Text Domain: conditional-smart-tags-for-wpforms
+* License: GPL-2.0-or-later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Exit if accessed directly.
+if( ! defined( 'ABSPATH' ) )
+	exit;
+
+class WPForms_Conditional_Smart_Tags
+{
+	/**
+	* Constructor to hook into WPForms smart tag processing.
+	*/
+	public function __construct()
+	{
+		add_filter( 'wpforms_process_smart_tags', [ $this, 'process_if_else_smart_tags' ], 10, 4 );
+	}
+
+	/**
+	* Callback to parse {if field_id="X" value="Y"} ... {else} ... {/if}.
+	*
+	* @param string $content   The content (notification, confirmation) containing smart tags.
+	* @param array  $form_data Data about the current form.
+	* @param array  $fields    Submitted fields data.
+	* @param int    $entry_id  The current entry ID.
+	*
+	* @return string Processed content with if/else blocks replaced accordingly.
+	*/
+	public function process_if_else_smart_tags( $content, $form_data, $fields, $entry_id )
+	{
+		$max_iterations = 10; // Prevent infinite loops
+		$iteration = 0;
+		
+		while(strpos($content, '{if') !== false && $iteration < $max_iterations)
+		{
+			$content = $this->process_innermost_if($content, $fields);
+			$iteration++;
+		}
+		
+		return $content;
+	}
+
+	/**
+	 * Process the innermost if statement in the content.
+	 * 
+	 * @param string $content The content to process
+	 * @param array $fields The form fields
+	 * @return string Processed content
+	 */
+	private function process_innermost_if($content, $fields)
+	{
+		// Find the innermost {if} that doesn't contain another {if}
+		$pattern = '/\{if\s+field_id="(\d+)"\s+value="([^"]*)"\}((?:(?!\{if).)*?)(?:\{else\}((?:(?!\{if).)*?))?\{\/if\}/s';
+		
+		return preg_replace_callback($pattern, function($matches) use ($fields) {
+			$field_id = $matches[1];
+			$required_value = $matches[2];
+			$if_content = $matches[3];
+			$else_content = isset($matches[4]) ? $matches[4] : '';
+
+			// Get the submitted value, if present
+			$submitted_value = '';
+			if(isset($fields[$field_id]['value']))
+				$submitted_value = trim($fields[$field_id]['value']);
+			
+			// Compare and return if_content or else_content
+			return ($submitted_value === $required_value) ? $if_content : $else_content;
+		}, $content);
+	}
+}
+
+$wpfcst_instance = new WPForms_Conditional_Smart_Tags();
---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== Conditional Smart Tags for WPForms ===
+Version: 1.0.0
+Stable tag: 1.0.0
+Requires at least: 5.2
+Tested up to: 6.7
+Requires PHP: 7.2
+Tags: conditional logic, conditional fields, wpforms, smart tags, forms
+Plugin URI: https://wordpress.org/plugins/conditional-smart-tags-for-wpforms
+Author: Maximum.Software
+Author URI: https://maximum.software
+License: GPL-2.0-or-later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a custom if-then-else smart tag syntax to conditionally show or hide content based on a field value in WPForms.
+
+== Description ==
+
+Conditional If/Then/Else Smart Tags for WPForms enhances your form by adding conditional logic capabilities through smart tags. This plugin allows you to display different content based on form field values.
+
+= Features =
+
+* Add if/then/else conditions where smart tags can be used
+* Compare field values using exact match conditions
+
+= Example Usage =
+
+Use this syntax in your WPForms notifications or confirmations:
+
+`{if field_id="3" value="TestValue"}
+This message is shown ONLY if field #3 exactly matches "TestValue".
+{else}
+This message is shown otherwise.
+{/if}`
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/conditional-smart-tags-for-wpforms` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress
+3. Use the new if/then/else smart tags in your WPForms notifications and confirmations
+
+== Frequently Asked Questions ==
+
+= How do I find a field's ID? =
+
+The field ID can be found in the WPForms form builder. When editing a field, look for the "Field ID" in the field options panel on the left.
+
+= Can I use multiple conditions? =
+
+Yes, you can use multiple if/then/else blocks. Just make sure to properly close each if block with {/if}.
+
+= Can I nest if/then/else blocks and other smart tags? =
+
+Yes, you can use nested if/then/else blocks and other smart tags.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
