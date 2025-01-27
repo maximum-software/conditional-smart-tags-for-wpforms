2c5a4d5e5fc014f5790552eb87f8bab0e7852ec8
Alex <alex@maximum.guru>
2025-01-26 17:57:08 -0800

    Added donate link

--- conditional-smart-tags-for-wpforms.php
+++ conditional-smart-tags-for-wpforms.php
@@ -8,6 +8,7 @@
 * Requires PHP: 7.2
 * Author: Maximum.Software
 * Author URI: https://maximum.software
+* Donate link: https://github.com/sponsors/maximum-software
 * Text Domain: conditional-smart-tags-for-wpforms
 * License: GPL-2.0-or-later
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
